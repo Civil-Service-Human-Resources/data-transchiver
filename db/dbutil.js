f5761a6bf44d75a2e330fd792d50fea2e29b954a
mmackiewicz1 <m.mackiewicz@kainos.com>
2020-05-13 14:10:37 +0200

    Implemented reconnection mechanism

--- db/dbutil.js
+++ db/dbutil.js
@@ -1,8 +1,35 @@
 const mysql = require('mysql');
 const util = require('util');
 
+function reconnect(connection, config){
+  console.log("New connection tentative...");
+
+  if (connection) {
+    connection.destroy();
+  }
+
+  var connection = mysql.createConnection(config);
+
+  connection.connect(function(error){
+      if (error) {
+          setTimeout(reconnect, 2000);
+      } else {
+          console.log("New connection established with the database.")
+          return connection;
+      }
+  });
+}
+
 let getMysql = ( config ) => {
-    const connection = mysql.createConnection( config );
+    const connection = mysql.createPool( config );
+
+    connection.on('error', function(error) {
+      console.log("Cannot establish a connection with the database: " + error.code);
+
+      if (err.code === "PROTOCOL_ENQUEUE_AFTER_QUIT") {
+        connection = reconnect(connection, config);
+      }
+    });
 
     return {
       query: util.promisify( connection.query ).bind( connection ),
